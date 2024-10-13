69a1df8ca7ea0e3cfe8f1c968df73962ffc31607
developerorin2002 <developerorin2002@gmail.com>
2024-10-13 21:01:24 +0600

    some change

--- src/Components/Task/Taskboard.jsx
+++ src/Components/Task/Taskboard.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import SearchBox from "./SearchBox";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
 
 export default function Taskboard() {
+
+    const initialTask = {
+        "id":crypto.randomUUID(),
+        "title":"Efficient Web API Connectivity in Python",
+        "description":"Develop a Python-based solution for connecting an API to a third-party database securely, focusing on efficient data handling and exchange.",
+        "tags":["web","python","api"],
+        "isFavourite":false
+    }
+
+    const [tasks , setTasks] = useState([initialTask]);
     
 
     return(
@@ -16,7 +27,7 @@ export default function Taskboard() {
                 {/* task action */}
 				<TaskAction/>
                 {/* task list */}
-				<TaskList/>
+				<TaskList tasks={tasks}/>
 			</div>
 		</div>
 	</section>
